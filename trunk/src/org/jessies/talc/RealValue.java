7178678d7976258a250ef5b8a91fb8fb02a6d7f9
enh@jessies.org <enh@jessies.org@7846e1d9-7c46-0410-93fb-7924d0d160c8>
2008-03-15 21:25:24 +0000

    src/org/jessies/talc/AstEvaluator.java: src/org/jessies/talc/BooleanValue.java: src/org/jessies/talc/Environment.java: src/org/jessies/talc/IntegerValue.java: src/org/jessies/talc/ListValue.java: src/org/jessies/talc/MapValue.java: src/org/jessies/talc/MatchValue.java: src/org/jessies/talc/NumericValue.java: src/org/jessies/talc/Parser.java: src/org/jessies/talc/RealValue.java: src/org/jessies/talc/Scope.java: src/org/jessies/talc/SymbolTable.java: src/org/jessies/talc/TalcType.java: update the copyright on various files I've touched this year.

--- trunk/src/org/jessies/talc/RealValue.java
+++ trunk/src/org/jessies/talc/RealValue.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Talc.
- * Copyright (C) 2007 Elliott Hughes <enh@jessies.org>.
+ * Copyright (C) 2007-2008 Elliott Hughes <enh@jessies.org>.
  * 
  * Talc is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
