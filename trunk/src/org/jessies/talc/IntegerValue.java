70c0271a383459bf82af2cd5f9fb03226318c62b
enh@jessies.org <enh@jessies.org@7846e1d9-7c46-0410-93fb-7924d0d160c8>
2008-10-14 06:43:21 +0000

    src/org/jessies/talc/IntegerValue.java: a fancier representation that avoids using BigInteger unless the integer is large enough to warrant one. This isn't much more complicated than the old implementation, but it's 5x faster.
    src/org/jessies/talc/JvmCodeGenerator.java: avoid String to BigInteger conversions for integer constants small enough to be represented as primitive longs.
    
    tests/compiler-trip.talc: new tests.
    
    demos/simple.talc: Cliff Click's original test, which takes 32s in Java using non-boxed primitive longs on my machine, 66s in Talc with these changes, and about 6 minutes before them. The factor of two for boxing is about what we've seen with RealValue, so this is probably as fast as we're going to get without actually using primitives.

--- trunk/src/org/jessies/talc/IntegerValue.java
+++ trunk/src/org/jessies/talc/IntegerValue.java
@@ -30,7 +30,24 @@ public class IntegerValue implements Comparable<IntegerValue> {
         }
     }
     
-    private final BigInteger value;
+    // If 'bignum' is null, this IntegerValue's value is 'fixnum'. Otherwise, it's 'bignum' and 'fixnum' is ignored.
+    private long fixnum;
+    private BigInteger bignum;
+    
+    // Internally, we often need to distinguish fixnum IntegerValues from bignum ones.
+    boolean isBig() {
+        return bignum != null;
+    }
+    
+    // Internally, we often want to treat an IntegerValue as if it was a bignum, whether it is or not.
+    private BigInteger big() {
+        return (bignum != null) ? bignum : BigInteger.valueOf(fixnum);
+    }
+    
+    // Used by JvmCodeGenerator. It is an error to call this on a bignum.
+    long longValue() {
+        return fixnum;
+    }
     
     public static IntegerValue valueOf(long l) {
         if (l >= -128 && l <= 127) {
@@ -40,82 +57,176 @@ public class IntegerValue implements Comparable<IntegerValue> {
     }
     
     public IntegerValue(String digits, int base) {
-        this.value = new BigInteger(digits, base);
-    }
-    
-    public IntegerValue(BigInteger value) {
-        this.value = value;
+        try {
+            this.fixnum = Long.parseLong(digits, base);
+        } catch (NumberFormatException ex) {
+            this.bignum = new BigInteger(digits, base);
+        }
     }
     
     private IntegerValue(long value) {
-        this.value = BigInteger.valueOf(value);
+        this.fixnum = value;
+    }
+    
+    private IntegerValue(BigInteger value) {
+        this.bignum = value;
+    }
+    
+    private static IntegerValue valueOf(BigInteger value) {
+        // Collapse to a fixnum if possible.
+        if ((value.bitLength() + 1) <= 64) {
+            return new IntegerValue(value.longValue());
+        }
+        return new IntegerValue(value);
     }
     
     public IntegerValue abs() {
-        return new IntegerValue(value.abs());
+        if (isBig() || fixnum == Long.MIN_VALUE) {
+            return new IntegerValue(big().abs());
+        } else {
+            return IntegerValue.valueOf(Math.abs(fixnum));
+        }
     }
     
     public IntegerValue add(IntegerValue rhs) {
-        return new IntegerValue(value.add(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().add(rhs.big()));
+        } else {
+            final long a = fixnum;
+            final long b = rhs.fixnum;
+            final long c = a + b;
+            if ((c ^ a) < 0 && (c ^ b) < 0) {
+                return new IntegerValue(big().add(rhs.big()));
+            } else {
+                return IntegerValue.valueOf(c);
+            }
+        }
     }
     
     public IntegerValue subtract(IntegerValue rhs) {
-        return new IntegerValue(value.subtract(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().subtract(rhs.big()));
+        } else {
+            final long a = fixnum;
+            final long b = rhs.fixnum;
+            final long c = a - b;
+            if ((c ^ a) < 0 && (c ^ ~b) < 0) {
+                return new IntegerValue(big().subtract(rhs.big()));
+            } else {
+                return IntegerValue.valueOf(c);
+            }
+        }
     }
     
     public IntegerValue multiply(IntegerValue rhs) {
-        return new IntegerValue(value.multiply(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().multiply(rhs.big()));
+        } else {
+            final long a = fixnum;
+            final long b = rhs.fixnum;
+            final long c = a * b;
+            if (a != 0 && (c / a) != b) {
+                return new IntegerValue(big().multiply(rhs.big()));
+            } else {
+                return IntegerValue.valueOf(c);
+            }
+        }
     }
     
     public IntegerValue divide(IntegerValue rhs) {
-        return new IntegerValue(value.divide(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().divide(rhs.big()));
+        } else {
+            return IntegerValue.valueOf(fixnum / rhs.fixnum);
+        }
     }
     
     public IntegerValue mod(IntegerValue rhs) {
-        return new IntegerValue(value.mod(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().mod(rhs.big()));
+        } else {
+            return IntegerValue.valueOf(fixnum % rhs.fixnum);
+        }
     }
     
     public IntegerValue shiftLeft(IntegerValue rhs) {
-        // FIXME: check that rhs not too large.
-        return new IntegerValue(value.shiftLeft(rhs.value.intValue()));
+        // FIXME: check that rhs not too large?
+        if (isBig() || rhs.isBig()) {
+            return new IntegerValue(big().shiftLeft(rhs.intValue()));
+        } else {
+            return IntegerValue.valueOf(fixnum << rhs.fixnum);
+        }
     }
     
     public IntegerValue shiftRight(IntegerValue rhs) {
-        // FIXME: check that rhs not too large.
-        return new IntegerValue(value.shiftRight(rhs.value.intValue()));
+        // FIXME: check that rhs not too large?
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().shiftRight(rhs.intValue()));
+        } else {
+            return IntegerValue.valueOf(fixnum >> rhs.fixnum);
+        }
     }
     
     public IntegerValue pow(IntegerValue rhs) {
-        // FIXME: check that rhs not too large.
-        return new IntegerValue(value.pow(rhs.value.intValue()));
+        // FIXME: check that rhs not too large?
+        // FIXME: special-case small enough fixnums?
+        return new IntegerValue(big().pow(rhs.intValue()));
     }
     
     public IntegerValue and(IntegerValue rhs) {
-        return new IntegerValue(value.and(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().and(rhs.big()));
+        } else {
+            return IntegerValue.valueOf(fixnum & rhs.fixnum);
+        }
     }
     
     public IntegerValue not() {
-        return new IntegerValue(value.not());
+        if (isBig()) {
+            return IntegerValue.valueOf(bignum.not());
+        } else {
+            return IntegerValue.valueOf(~fixnum);
+        }
     }
     
     public IntegerValue or(IntegerValue rhs) {
-        return new IntegerValue(value.or(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().or(rhs.big()));
+        } else {
+            return IntegerValue.valueOf(fixnum | rhs.fixnum);
+        }
     }
     
     public IntegerValue xor(IntegerValue rhs) {
-        return new IntegerValue(value.xor(rhs.value));
+        if (isBig() || rhs.isBig()) {
+            return IntegerValue.valueOf(big().xor(rhs.big()));
+        } else {
+            return IntegerValue.valueOf(fixnum ^ rhs.fixnum);
+        }
     }
     
     public IntegerValue negate() {
-        return new IntegerValue(value.negate());
+        if (isBig() || fixnum == Long.MIN_VALUE) {
+            return new IntegerValue(big().negate());
+        } else {
+            return IntegerValue.valueOf(-fixnum);
+        }
     }
     
     public IntegerValue decrement() {
-        return new IntegerValue(value.subtract(BigInteger.ONE));
+        if (isBig() || fixnum == Long.MIN_VALUE) {
+            return new IntegerValue(big().subtract(BigInteger.ONE));
+        } else {
+            return IntegerValue.valueOf(fixnum - 1);
+        }
     }
     
     public IntegerValue increment() {
-        return new IntegerValue(value.add(BigInteger.ONE));
+        if (isBig() || fixnum == Long.MAX_VALUE) {
+            return new IntegerValue(big().add(BigInteger.ONE));
+        } else {
+            return IntegerValue.valueOf(fixnum + 1);
+        }
     }
     
     /**
@@ -123,54 +234,82 @@ public class IntegerValue implements Comparable<IntegerValue> {
      * The suggested idiom for performing any boolean comparison 'op' is: (x.compareTo(y) op 0).
      */
     public int compareTo(IntegerValue rhs) {
-        return value.compareTo(rhs.value);
+        if (isBig() || rhs.isBig()) {
+            return big().compareTo(rhs.big());
+        } else {
+            if (fixnum < rhs.fixnum) {
+                return -1;
+            } else if (fixnum == rhs.fixnum) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
     }
     
     public IntegerValue signum() {
-        return IntegerValue.valueOf(value.signum());
+        if (isBig()) {
+            return IntegerValue.valueOf(big().signum());
+        } else {
+            return IntegerValue.valueOf(Long.signum(fixnum));
+        }
     }
     
     @Override public boolean equals(Object o) {
-        if (o instanceof IntegerValue) {
-            return value.equals(((IntegerValue) o).value);
+        if (o instanceof IntegerValue == false) {
+            return false;
         }
-        return false;
+        final IntegerValue rhs = (IntegerValue) o;
+        // We normalize in valueOf, so we know that equal values must either both be big or both be fix.
+        if (isBig() || rhs.isBig()) {
+            return big().equals(rhs.big());
+        } else {
+            return fixnum == rhs.fixnum;
+        }
     }
     
     @Override public int hashCode() {
-        return value.hashCode();
+        if (isBig()) {
+            return big().hashCode();
+        } else {
+            return (int)(fixnum ^ (fixnum >>> 32));
+        }
     }
     
     public IntegerValue factorial() {
-        if (value.signum() < 0) {
+        final BigInteger n = big();
+        if (n.signum() < 0) {
             throw new IllegalArgumentException("factorial requires a non-negative integer argument; got " + this + " instead");
         }
-        BigInteger n = value;
         BigInteger result = BigInteger.ONE;
         BigInteger i = BigInteger.ONE;
         while (i.compareTo(n) < 0) {
             i = i.add(BigInteger.ONE);
             result = result.multiply(i);
         }
-        return new IntegerValue(result);
+        return IntegerValue.valueOf(result);
     }
     
     public Object toNativeJavaObject() {
-        return value;
+        // FIXME: this isn't necessarily appropriate. We might want to throw an exception instead.
+        if (isBig()) {
+            return big();
+        } else {
+            return Long.valueOf(fixnum);
+        }
     }
     
     public int intValue() {
-        return value.intValue();
+        return isBig() ? big().intValue() : (int) fixnum;
     }
     
-    /** Returns the equivalent IntegerValue, or throws an exception. */
     public IntegerValue to_i() {
         return this;
     }
     
     /** Returns the equivalent RealValue, or throws an exception. */
     public RealValue to_r() {
-        double result = value.doubleValue();
+        double result = isBig() ? big().doubleValue() : (double) fixnum;
         if (result == Double.NEGATIVE_INFINITY || result == Double.POSITIVE_INFINITY) {
             throw new RuntimeException("Integer value too large");
         }
@@ -178,14 +317,26 @@ public class IntegerValue implements Comparable<IntegerValue> {
     }
     
     public String toString() {
-        return value.toString();
+        if (isBig()) {
+            return big().toString();
+        } else {
+            return Long.toString(fixnum);
+        }
     }
     
     public String to_base(IntegerValue base) {
-        return value.toString(base.intValue());
+        if (isBig() || base.isBig()) {
+            return big().toString(base.intValue());
+        } else {
+            return Long.toString(fixnum, (int) base.fixnum);
+        }
     }
     
     public String to_char() {
-        return String.valueOf((char) intValue());
+        if (isBig()) {
+            return String.valueOf((char) intValue());
+        } else {
+            return String.valueOf((char) fixnum);
+        }
     }
 }
